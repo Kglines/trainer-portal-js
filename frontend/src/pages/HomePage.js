4fd57b3565278a011501d3d17e5d397c26a40f31
Keith Glines <keithglines@yahoo.com>
2023-07-01 11:11:48 -0400

    Adding Announcements to Home Page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -7,11 +7,33 @@ const HomePage = () => {
 
   if(!sessionUser) return <Redirect to='/'/>
   return (
-    <div className='sm:w-full md:w-5/6 bg-white mx-auto text-center h-auto'>
+    <div className='sm:w-full md:w-5/6 bg-white mx-auto text-center min-h-auto'>
       <p className='text-4xl font-bold py-8'>Welcome {sessionUser.firstname}!</p>
       <div>
         <div>
-          <p>Announcements</p>
+          <p className='text-xl underline'>Announcements</p>
+        </div>
+        <div className='my-8 pb-24'>
+          <ul>
+            <li>
+              <p>6/12 - this thing is going to happen</p>
+            </li>
+            <li>
+              <p>6/18 - this other thing is going to happen</p>
+            </li>
+            <li>
+              <p>6/20 - no thing is going on today</p>
+            </li>
+            <li>
+              <p>6/12 - this thing is going to happen</p>
+            </li>
+            <li>
+              <p>6/18 - this other thing is going to happen</p>
+            </li>
+            <li>
+              <p>6/20 - no thing is going on today</p>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
