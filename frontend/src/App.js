b4ac3e2ed264f19783a07e938d892470aabe0394
Keith Glines <keithglines@yahoo.com>
2023-07-01 08:00:02 -0400

    Add Tailwind and Update Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,9 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage';
+import LoginFormPage from './pages/LoginFormPage';
+import SignupFormPage from './pages/SignupFormPage';
+import HomePage from './pages/HomePage';
 import * as sessionActions from './store/session';
 
 function App() {
@@ -11,11 +13,17 @@ function App() {
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
-  
+
   return (
     <Switch>
-      <Route path='/'>
+      <Route path='/' exact>
         <LoginFormPage />
+      </Route>
+      <Route path='/signup' exact>
+        <SignupFormPage />
+      </Route>
+      <Route path='/home' exact>
+        <HomePage />
       </Route>
     </Switch>
   );
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -32,6 +32,24 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+// Signup
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch('/api/users', {
+    method: 'POST',
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 // Restore the user
 export const restoreUser = () => async (dispatch) => {
   const response = await csrfFetch('/api/session');
