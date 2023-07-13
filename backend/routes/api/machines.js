c4c1bc06f7b60fbda536df5b0131f05c4bd23003
Keith Glines <keithglines@yahoo.com>
2023-07-13 11:20:57 -0400

    Machines API

--- backend/routes/api/machines.js
+++ backend/routes/api/machines.js
@@ -22,4 +22,44 @@ router.post('', requireAuth, async (req, res) => {
     res.json(machine)
 })
 
+router.put('/:machineId', requireAuth, async(req, res) => {
+    const { machineId } = req.params;
+    const { number, type, manufacturer, name, machineImg } = req.body;
+
+    const machine = await Machine.findByPk(machineId);
+
+    if(machine){
+        await machine.update({
+            number,
+            type,
+            manufacturer,
+            name,
+            machineImg
+        });
+        res.json(machine)
+    } else {
+        const error = new Error("Machine couldn't be found.");
+        error.status = 404;
+        throw error
+    }
+})
+
+router.delete('/:machineId', requireAuth, async (req, res) => {
+    const { machineId } = req.params;
+
+    const machine = await Machine.findByPk(machineId);
+
+    if(machine){
+        await machine.destroy();
+        return res.json({
+            message: 'Successfully deleted.',
+            statusCode: 200
+        })
+    } else {
+        const error = new Error("Machine couldn't be found.");
+        error.status = 404;
+        throw error;
+    }
+})
+
 module.exports = router;
