df72de5a63735a89ffc50a0ec8d0efeb002dd381
Keith Glines <keithglines@yahoo.com>
2023-07-18 07:03:33 -0400

    Maintenance Store completed

--- frontend/src/store/maintenance.js
+++ frontend/src/store/maintenance.js
@@ -1,7 +1,13 @@
 import { csrfFetch } from './csrf';
 
+// Action Variables
 const GET_MAINTENANCE = 'maintenance/get';
+const CREATE_MAINTENANCE = 'maintenance/create';
+const UPDATE_MAINTENANCE = 'maintenance/update';
+const DELETE_MAINTENANCE = 'maintenance/delete';
 
+// Actions
+// GET
 export const getMaintenance = (maintenance) => {
     return {
         type: GET_MAINTENANCE,
@@ -9,6 +15,31 @@ export const getMaintenance = (maintenance) => {
     };
 };
 
+// CREATE
+export const createMaintenance = (maintenance) => {
+    return {
+        type: CREATE_MAINTENANCE,
+        payload: maintenance
+    }
+}
+
+// UPDATE
+export const updateMaintenance = (maintenance) => {
+    return {
+        type: UPDATE_MAINTENANCE,
+        payload: maintenance
+    };
+};
+
+export const deleteMaintenance = (maintenance) => {
+    return {
+        type: DELETE_MAINTENANCE,
+        payload: maintenance
+    };
+};
+
+// Thunks
+// GET
 export const fetchGetMaintenance = () => async (dispatch) => {
     const res = await csrfFetch('/api/maintenance');
 
@@ -20,6 +51,52 @@ export const fetchGetMaintenance = () => async (dispatch) => {
     return res;
 }
 
+// CREATE
+export const fetchCreateMaintenance = (maintenance) => async (dispatch) => {
+    const res = csrfFetch('/api/maintenance', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(maintenance)
+    });
+
+    if(res.ok){
+        const maintenance = await res.json();
+        dispatch(createMaintenance(maintenance));
+        return maintenance;
+    };
+    return res;
+};
+
+// UPDATE
+export const fetchUpdateMaintenance = (maintenance) => async (dispatch) => {
+    const res = await csrfFetch(`/api/maintenance/${maintenance.id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(maintenance)
+    });
+
+    if(res.ok){
+        const maintenance = await res.json();
+        dispatch(updateMaintenance(maintenance));
+        return maintenance;
+    };
+    return res;
+};
+
+// DELETE
+export const fetchDeleteMaintenance = (maintenanceId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/maintenance/${maintenanceId}`, {
+        method: 'DELETE'
+    });
+
+    if(res.ok){
+        const maintenance = await res.json();
+        dispatch(deleteMaintenance(maintenance));
+        return maintenance;
+    };
+    return res;
+};
+
 // Reducer
 const initialState = {};
 
@@ -29,6 +106,15 @@ const maintenanceReducer = (state = initialState, action) => {
     switch(action.type){
         case GET_MAINTENANCE:
             action.payload.maintenance.forEach(maintenance => newState[maintenance.id] = maintenance);
+            return newState;
+        case CREATE_MAINTENANCE:
+            newState = { ...state, [action.payload.id]: action.payload };
+            return newState;
+        case UPDATE_MAINTENANCE:
+            newState = action.payload;
+            return newState;
+        case DELETE_MAINTENANCE:
+            delete newState[action.payload];
             return newState;
         default:
             return newState
